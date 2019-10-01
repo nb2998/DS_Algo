de0863d3dfbd88cc191cf3871ba51eb43f66c09c
Piyush Mamidwar <mamidwar.piyush@gmail.com>
2019-10-01 20:20:34 +0530

    Update TrieClient.java

--- Trie/TrieClient.java
+++ Trie/TrieClient.java
@@ -1,10 +1,24 @@
-
+package trie;
 
 public class TrieClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Trie trie  = new Trie();
+		trie.insert("the");
+		trie.insert("a");
+		trie.insert("there");
+		trie.insert("answer");
+		trie.insert("any");
+		trie.insert("by");
+		trie.insert("bye");
+		trie.insertRecursive("their");
+		System.out.println(trie.search("answer"));
+		trie.delete("any");
+		System.out.println(trie.search("any"));//prints false
+
+		
 	}
 
 }
