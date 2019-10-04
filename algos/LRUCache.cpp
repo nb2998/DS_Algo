4becf4a3b3d35f7cab79985d1908e031c0395efa
16je002197 <banipreet.raheja@gmail.com>
2019-10-04 16:29:02 +0530

    Comments added to LRUCache

--- algos/LRUCache.cpp
+++ algos/LRUCache.cpp
@@ -1,26 +1,35 @@
+// Implemented LRU Cache, most famous algorithm followed in Page Replacement Techniques.
+// Here we are doing search in O(1) using Hashing - map in CPP is used for Hashing
+// For deletion of a node and insertion in O(1) time, a doubly linked list is used.
+// Basic implementation of a LRU Cache uses a Stack Like Datastructure which is implemented using Doubly Linked List and HashMap. The most recent page is at the top of the stack and the least recent is at the bottoms
+
+// class node is used for implementing a single node of the doubly linked list.
 class node
 {
     public:
-    node* prev;
-    node* next;
-    int key;
-    int val;
-    node(int x, int y)
+    node* prev; // points to previous node in the list
+    node* next; // points to next node in the list
+    int key; // contains the key of the node which will be used to refer the node
+    int val; // contains the value of the node
+    node(int x, int y) // constructor
     {
-        key = x;
-        val = y;
-        prev = NULL;
+        key = x; // initialized key as x
+        val = y; // initialized val as y
+        prev = NULL; // prev and next are NULL for an isolated node
         next = NULL;
     }
 };
+
+// class LRUCache contains the base logic for LRU Cache Implementation
 class LRUCache {
 public:
-    map<int, node*> look;
-    node* head;
-    node* tail;
-    int curr;
-    int cap;
-    LRUCache(int capacity) {
+    map<int, node*> look; // look is a Hashmap which is used to do fast search of the node having a particular key
+    node* head; // head points to the head of the doubly linked list
+    node* tail; // tail points to the last node of the doubly linked list
+    int curr; 
+    int cap; // variable that states the capacity of the LRU Cache. If the size of the cache exceed cap, last node of the linked list is popped from the cache.
+    /* Constructor to initialize the LRU Cache */
+	LRUCache(int capacity) {
         curr = 0;
         cap = capacity;
         head = NULL;
@@ -29,11 +38,13 @@ public:
         look = empt;
     }
     
+    
+    // Function to get the value of the node having key x
     int get(int x) {
-        if(look[x]!=NULL)
+        if(look[x]!=NULL) // if there is such node having key as x
      {
          auto nd = look[x];
-         if(nd->prev)
+         if(nd->prev) // delete this accessed node from the current position and insert it at the head
          {
              auto temp = nd->prev;
              auto ntemp = nd->next;
@@ -52,12 +63,14 @@ public:
          }
          return nd->val;
      }
-     return -1;
+     return -1; // if no such node having key x is found
     }
     
+    // Function to place a node having key x and value y
     void put(int x, int y) {
-        if(look[x]!=NULL)
+        if(look[x]!=NULL) // if already such node having key x exists in cache
          {
+         	//delete the node from its position and place it at head
              auto nd = look[x];
              if(nd->prev)
              {
@@ -79,6 +92,7 @@ public:
              nd->val = y;
              return;
          }
+         // if no such node having key x exists in the cache
          node* nd = new node(x,y);
          look[x] = nd;
          if(head==NULL)
@@ -93,9 +107,10 @@ public:
          head = nd;
          head->prev = NULL;
          ++curr;
-         if(curr > cap)
+         if(curr > cap) // if the capacity after insertion of new node exceeds the limit
          {
              --curr;
+             //delete the element at tail
              if(tail)
              {
                  auto temp = tail;
