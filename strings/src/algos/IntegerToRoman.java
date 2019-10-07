ad0468ea6be3fa027b1bea5563b239aa0aede270
Tarun Gulati <tarun_gulati@intuit.com>
2019-10-06 19:40:21 -0700

    Integer to roman numberal string algo implementaiton in java

--- strings/src/algos/IntegerToRoman.java
+++ strings/src/algos/IntegerToRoman.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class IntegerToRoman {
     public String intToRoman(int num) {
         StringBuilder sb = new StringBuilder();
-        Map<Integer,String> map = new HashMap<>();
+        Map<Integer, String> map = new HashMap<>();
         map.put(1000, "M");
         map.put(900, "CM");
         map.put(500, "D");
@@ -20,9 +20,9 @@ public class IntegerToRoman {
         map.put(5, "V");
         map.put(4, "IV");
         map.put(1, "I");
-        int[] values = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        for(int i = 0; i < values.length; i++) {
-            while(num >= values[i]) {
+        int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
                 num -= values[i];
                 sb.append(map.get(values[i]));
             }
