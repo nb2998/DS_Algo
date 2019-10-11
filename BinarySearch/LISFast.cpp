10124a28ebb2fe416da3190052ed3429dc333632
16je002197 <banipreet.raheja@gmail.com>
2019-10-11 14:21:52 +0530

    Updated FastLIS

--- BinarySearch/LISFast.cpp
+++ BinarySearch/LISFast.cpp
@@ -1,29 +1,12 @@
 #include<bits/stdc++.h>
-#define Loop(i,a,b) for(int i=a;i<b;++i)
-#define NLoop(i,a,b) for(int i=a;i>=b;--i)
-#define print(arr,n,m) for(int i=0;i<n;++i) { for(int j=0;j<m;++j) cout<<arr[i][j]<<" "; cout<<endl; }
-#define print2(arr,n) for(int i=0;i<n;++i) { cout<<arr[i]<<" "; } cout<<endl;
-#define mp(a,b) make_pair(a,b)
-#define MOD 1000000007
-#define pb(x) push_back(x)
-#define ub(x,el) upper_bound(x.begin(),x.end(),el) - x.begin()
-#define lb(x,el) lower_bound(x.begin(),x.end(),el) - x.begin();
-#define bs(x,el) binary_search(x.begin(),x.end(),el)
 using namespace std;
 typedef long long int ll;
-ll gcd(ll a,ll b) { if (b == 0) return a; return gcd(b, a % b);  }
-
-ll power(ll x,ll y, ll p) { ll res = 1; x = x % p; while (y > 0) { if (y & 1) res = (res*x) % p; y = y>>1; x = (x*x) % p;   } return res; } 
-//class compare { public: bool operator() (ll a,ll b) { return a>b;};
 int main()
 {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     ll t;
     cin>>t;
     while(t--)
-    {
-		
+    {		
 		ll n;
 		cin>>n;
 		vector<ll> arr(n);
@@ -39,7 +22,6 @@ int main()
 			ll hi = length-1;
 			while(lo<=hi)
 			{
-			//	cout<<lo<<" "<<hi<<endl;
 				ll mid = (lo+hi)>>1;
 				ll j = tail[mid];
 				if(arr[j] < arr[i]) lo = mid+1;
@@ -50,7 +32,6 @@ int main()
 			if(pos==0) par[i] = -1;
 			else par[i] = tail[pos-1];
 			if(pos==length) ++length;
-		//	print2(tail,tail.size());
 		}
 		cout<<length<<endl;
 		stack<ll> st;
