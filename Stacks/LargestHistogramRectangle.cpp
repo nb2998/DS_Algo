c8438d5f4a369324612b06a8a4ff00a4c26466cb
Himanshu Singh <singh.himanshu242@gmail.com>
2019-10-11 17:31:35 +0530

    largestRectangle

---
+++ Stacks/LargestHistogramRectangle.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int largestRectangleArea(vector<int> &A) {
+    int n = A.size();
+    if(n==0) return 0;
+    stack<int> st;
+    st.push(0);
+    int ans = 0;
+    for(int i=1;i<A.size();++i)
+    {
+        while(!st.empty())
+        {
+            int tos = st.top();
+            if(A[tos] <= A[i]) break;
+            st.pop();
+            if(st.empty()) ans = max(ans, A[tos]*(i));
+            else ans = max(ans, A[tos]*(i-st.top()-1));
+        }
+        st.push(i);
+    }
+    while(!st.empty())
+    {
+        int tos = st.top();
+        st.pop();
+        if(st.empty()) ans = max(ans, A[tos]*n);
+        else ans = max(ans, A[tos]*(n-st.top()-1));
+    }
+    return ans;
+}
+//Driver Code
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int> arr(n);
+	for(int i=0;i<n;++i) cin>>arr[i];
+	cout<<largestRectangleArea(arr)<<endl;
+}
