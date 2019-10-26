0e0063238f4a0fb62ba76ffb486d21592391052d
Shilpi Priya <48611488+shilpipriya@users.noreply.github.com>
2019-10-26 23:15:39 +0530

    Update SBank.cpp

--- Sorting/SBank.cpp
+++ Sorting/SBank.cpp
@@ -17,8 +17,6 @@ int main()
            v.push_back(str);
         }
         sort(v.begin(),v.end());
-        /*for(int i=0;i<v.size();i++)
-        cout<<v[i]<<endl;*/
         map<string,int>m;
         for(int i=0;i<v.size();i++)
         m[v[i]]++;
