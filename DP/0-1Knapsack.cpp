58911468d3a0505898d2b5b37bb88598bafa947d
Shiv Pratap Singh <shiv.si@media.net>
2020-10-03 11:16:58 +0530

    indent code properly & reduce space complexity to linear

--- DP/0-1Knapsack.cpp
+++ DP/0-1Knapsack.cpp
@@ -3,25 +3,15 @@ using namespace std;
 
 int max(int a, int b) { return (a > b)? a : b; } 
 
-int knapSack(int W, int wt[], int val[], int n) 
-{ 
-int i, w; 
-int dp[n+1][W+1]; 
-
-for (i = 0; i <= n; i++) 
-{ 
-	for (w = 0; w <= W; w++) 
-	{ 
-		if (i==0 || w==0) 
-			dp[i][w] = 0; 
-		else if (wt[i-1] <= w) 
-				dp[i][w] = max(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w]); 
-		else
-				dp[i][w] = dp[i-1][w]; 
-	} 
-} 
-
-return dp[n][W]; 
+int knapSack(int W, int wt[], int val[], int n) {  
+    int dp[W+1];
+    memset(dp, 0, sizeof(dp));
+    for (int i = 0; i <= n; i++) { 
+        for(int j=W; j>=wt[i]; j--) {
+            dp[j] = max(dp[j] , val[i] + dp[j-wt[i]]);
+	    } 
+    } 
+    return dp[W]; 
 } 
 
 int main() 
